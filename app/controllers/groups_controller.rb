3eb5ac018a68940507657693f87f4beb0958543b
dennis <denniss@berkeley.edu>
2010-11-01 00:35:45 -0700

    pushing

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,19 @@
 class GroupsController < ApplicationController
   def index
+    @groups = Group.find(:all)
   end
 
   def show
+    
   end
 
   def new
+    @group = Group.new
+    
   end
 
   def create
+    
   end
 
 end
