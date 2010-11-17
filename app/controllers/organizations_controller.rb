e2e1d4d9d4f1d8ab8936f592a7dd560da6e57f19
dennis <denniss@berkeley.edu>
2010-11-17 07:02:24 -0800

    COMMIT

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -9,11 +9,7 @@ class OrganizationsController < ApplicationController
     @org = Organization.new
     @org.name = params[:organization][:name]
     @org.description = params[:organization][:description]
-    if @current_user != nil
-      @org.creator_id = @current_user.id
-    else
-      @org.creator_id = 4
-    end
+    @org.creator_id = @current_user.id
     
     if @org.save
       flash[:notice] = "#{params[:organization][:name]} has been created"
@@ -25,17 +21,9 @@ class OrganizationsController < ApplicationController
   end
 
   def index
-    if @current_user != nil
-      @id = @current_user.id
-      @joined_orgs = @current_user.organizations #organizations that the current user is a part of
-    else
-      @id = 4
-      @joined_orgs = []
-    end
-      
+    @id = @current_user.id
+    @joined_orgs = @current_user.organizations #organizations that the current user is a part of      
     @my_orgs = Organization.find_by_creator_id(@id.id) #organizations that the current user created
-
-
   end
 
   def show
