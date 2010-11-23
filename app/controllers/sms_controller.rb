c98f4005f13cdb432ba69ae415dc7dfddc661f15
James <spitfire67@berkeley.edu>
2010-11-23 03:32:01 -0800

    got sms working

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -2,15 +2,13 @@ class SmsController < ApplicationController
   before_filter :require_user, :except => [:inbound_sms]
   def inbound_sms
     #TODO: clean up
-    @user = User.find_by_phone_number(params[:From].scan(/[0-9]+/).first[1..100]) #remove the +1...
-    message_body = params[:Body]
-    logger.info("hello")
-    logger.info(params[:Body])
-    @sms_processor = SmsProcessor.new(SmsSession.get_sms_session(@user.phone_number), message_body)
+    from_phone_number = params[:From].scan(/[0-9]+/).first[1..100]
+    @user = User.find_by_phone_number from_phone_number
+    @sms_processor = SmsProcessor.new(SmsSession.get_sms_session(@user.phone_number), params[:body])
     @sms_processor.process_message
     account = Twilio::RestAccount.new(ACCOUNT_SID, ACCOUNT_TOKEN)
     logger.info(@sms_processor.response_message)
-    h = {:From => "510-550-5285", :To => "440-554-8235", :Body => @sms_processor.response_message}
+    h = {:From => "510-550-5285", :To => from_phone_number, :Body => @sms_processor.response_message}
     resp = account.request("/#{API_VERSION}/Accounts/#{ACCOUNT_SID}/SMS/Messages", 'POST', h)
     render :nothing => true
   end
