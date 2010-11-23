9b8aaf2339a0ffbee90e14034a024cc59ddfbdf7
James <spitfire67@berkeley.edu>
2010-11-23 02:08:32 -0800

    sms_controller bug resolved

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -4,7 +4,7 @@ class SmsController < ApplicationController
     #TODO: clean up
     @user = User.find_by_phone_number(params[:From].scan(/[0-9]+/).first[1..100]) #remove the +1...
     message_body = params[:Body].strip
-    @sms_processor = SmsProcessor.new(User.find_by_phone_number(@user.phone_number), message_body)
+    @sms_processor = SmsProcessor.new(SmsSession.get_sms_session(@user.phone_number), message_body)
     account = Twilio::RestAccount.new(ACCOUNT_SID, ACCOUNT_TOKEN)
     h = {:From => "415-599-2671", :To => n, :Body => @sms_processor.resonse_message}
     resp = account.request("/#{API_VERSION}/Accounts/#{ACCOUNT_SID}/SMS/Messages", 'POST', h)
--- app/models/sms_session.rb
+++ app/models/sms_session.rb
@@ -9,4 +9,13 @@ class SmsSession < ActiveRecord::Base
     ((Time.now - Time.parse(updated_at.to_s)).to_i / 60) < SMS_SESSION_VALID_FOR
   end
 
+  def self.get_sms_session(phone_number)
+    existing_sess = SmsSession.find_by_phone_number phone_number
+    if existing_sess
+      existing_sess
+    else
+      SmsSession.create(:phone_number => phone_number)
+    end
+  end
+
 end
