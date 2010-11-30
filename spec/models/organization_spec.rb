5bd31e79aca816220a6438d3702dc759a66d07ac
dennis <denniss@berkeley.edu>
2010-11-30 09:30:04 -0800

    adding project spec

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -3,10 +3,8 @@ require 'spec_helper'
 describe Organization do
   
   before :each do
-    Factory.create(:user, :email => "dennis@dennis.com", :id => 2009)
-    Factory.create(:organization, :id => 20, :name => "dennis.com", :description => "Dennis Company", :creator_id => 2009)
-    @organization_owner = User.find(2009)
-    @organization = Organization.find(20)
+    @organization_owner = Factory.create(:user, :email => "dennis@dennis.com")
+    @organization = Factory.create(:organization, :name => "dennis.com", :description => "Dennis Company", :creator_id => @organization_owner.id)
   end
   
   describe "user as the creator of an organization" do
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @project_count = Project.count
+    @organization = Factory.create(:organization, :name => "Communist Party", :name => "Fooood")
+    @project = Factory.create(:project, :name => "Dennis Project", :description => "Gotta finish this shit", :organization_id => @organization.id)
+  end
+  
+  describe "Project Creation" do
+    it "should give the correct project count" do
+      Project.count.should == (@project_count + 1)
+    end
+    
+    it "should return the right project name" do
+      @project.name.should == "Dennis Project"
+    end
+    
+    it "should return the right description" do
+      @project.description.should == "Gotta finish this shit"
+    end
+  end
+  
+  describe "Association with Organization" do
+    
+    
+    
+  end
 end
