5945135e2e47841b0dc72f25f700e93a85389d69
dennis <denniss@berkeley.edu>
2010-12-04 00:17:36 -0800

    adfas

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,9 +1,8 @@
 class Organization < ActiveRecord::Base
-  has_many :user_organizations
   has_many :users, :through => :user_organizations
-  has_many :projects
-  has_many :groups
+  has_many :projects, :dependent => :destroy
+  has_many :groups, :dependent => :destroy
   #has_many :tasks
-  has_many :invitations
+  has_many :invitations, , :dependent => :destroy
   validates_presence_of :name, :description
 end
