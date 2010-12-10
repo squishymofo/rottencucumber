6cb1e16da9ffa7397bb4d24501e7e4ea3092f110
James <spitfire67@berkeley.edu>
2010-12-10 03:25:12 -0800

    Fixed seeds for comment over sms integration testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,22 +2,37 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 #   Create me
-u = User.new(:email => "spitfire67@berkeley.edu", :password => "password", :password_confirmation => "password", :first_name => "James")
-u.first_name = "James"
-u.active = true
-u.save(false)
-u.active = true
-u.save
-u.phone_number = "4405548235"
-u.sms_enabled = true
-u.save
-org = Organization.create(:name => "my org", :description => "desc of my org", :creator_id => u.id) # create an org
-u.organizations << org # this is my org
-u.save
-g = Group.create(:name => "test groups", :organization_id => org.id) # create a group within the org
-UserGroup.create(:user_id => User.find_by_phone_number("4405548235").id, :group_id => g.id) # add the me to the group
-p = Project.create(:name => "do work", :organization_id => org.id, :description => "description") # create a project to add some tasks to
-num_tasks = "2"
-(1..num_tasks.to_i).each do |i| #now add some active tasks to the project
-  Task.create(:name => "Thing #{i}", :description => "Description of task #{i}", :group_id => g.id, :project_id => p.id, :status => 1)
+def make_active_user_with_sms_enabled
+  u = User.new(:email => "spitfire67@berkeley.edu", :password => "password", :password_confirmation => "password", :first_name => "James")
+  u.first_name = "James"
+  u.active = true
+  u.save(:validate => false)
+  u.active = true
+  u.save
+  u.phone_number = "4405548235"
+  u.sms_enabled = true
+  u.save
+  u
 end
+
+def make_group_for_user(user)
+  g = Group.create!(:name => "test_group")
+  user.groups << g
+  user.save(:validate => false)
+  g
+end
+
+def make_task_for_group(group, task_name)
+  Task.create(:group_id => group.id, :name => task_name, :status => 1, :description => "the desc")
+end
+
+@user = make_active_user_with_sms_enabled
+@org = Organization.create(:name => "my org", :description => "desc of my org") # create an org
+@user.organizations << @org # this is my org
+@user.save
+@g = make_group_for_user(@user)
+@t = make_task_for_group(@org, "task 1")
+@p = Project.create(:name => "do work", :organization_id => @org.id, :description => "description")
+@t.project_id = @p.id
+@t.save
+
