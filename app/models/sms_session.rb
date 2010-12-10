8a0b56cac396e49f900c136318c8d46b0a3f792a
James <spitfire67@berkeley.edu>
2010-12-10 05:26:05 -0800

    fixed sms_session

--- app/models/sms_session.rb
+++ app/models/sms_session.rb
@@ -6,6 +6,19 @@ class SmsSession < ActiveRecord::Base
     User.find_by_phone_number phone_number
   end
 
+  def active?
+  ((Time.now - Time.parse(updated_at.to_s)).to_i / 60) < SMS_SESSION_VALID_FOR
+  end
+
+  def self.get_sms_session(phone_number)
+    existing_sess = SmsSession.find_by_phone_number phone_number
+    if existing_sess
+      existing_sess
+    else
+      SmsSession.create(:phone_number => phone_number)
+    end
+  end
+
   def self.get_sms_session(phone_number)
     existing_sess = SmsSession.find_by_phone_number phone_number
     existing_sess
