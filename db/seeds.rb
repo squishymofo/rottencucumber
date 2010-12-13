f3bb6d36a2178622c1593bbbdffb20eddd992a7b
James <spitfire67@berkeley.edu>
2010-12-13 13:26:33 -0800

    Fixed landing page view bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,8 +24,8 @@ def make_group_for_users(users_a)
   g
 end
 
-def make_task_for_group(group, task_name)
-  Task.create(:group_id => group.id, :name => task_name, :status => 1, :description => "the desc")
+def make_task_for_group(group, task_name, proj)
+  Task.create(:group_id => group.id, :name => task_name, :status => 1, :description => "the desc", :project_id => proj)
 end
 
 @user = make_active_user_with_sms_enabled("spitfire67@berkeley.edu", "James", "4405548235")
@@ -34,9 +34,6 @@ end
 @user.organizations << @org # this is my org
 @user.save
 @g = make_group_for_users([@user, @user2])
-@t = make_task_for_group(@org, "Buy rubber tubing")
-@t = make_task_for_group(@org, "Clean up the back porch")
 @p = Project.create(:name => "do work", :organization_id => @org.id, :description => "description")
-@t.project_id = @p.id
-@t.save
-
+@t = make_task_for_group(@org, "Clean up the back porch", @p)
+@t = make_task_for_group(@org, "Buy rubber tubing", @p)
